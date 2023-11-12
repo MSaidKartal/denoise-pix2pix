fb8264d2fc6a890582f038a0e2280be07bbda958
MSaidKartal <78637906+MSaidKartal@users.noreply.github.com>
2023-11-12 18:34:21 +0300

    Update plot_utils.py

--- plot_utils.py
+++ plot_utils.py
@@ -3,19 +3,6 @@ from matplotlib.patches import PathPatch, Rectangle
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 
-
-def _draw_line(ax, coords, clr='g'):
-    line = Path(coords, [Path.MOVETO, Path.LINETO])
-    pp = PathPatch(line, linewidth=3, edgecolor=clr, facecolor='none')
-    ax.add_patch(pp)
-
-def _set_axes_labels(ax, axes_x, axes_y):
-    ax.set_xlabel(axes_x)
-    ax.set_ylabel(axes_y)
-    ax.set_aspect('equal', 'box')
-
-_rec_prop = dict(linewidth=5, facecolor='none')
-
 def show_volume(vol, z, fig_size=(14, 7)):
     #print(vol[0].shape)
     #print(vol[1].shape)
@@ -42,3 +29,39 @@ def interactive_show(vol):
         z=IntSlider(min=0, max=vol_shape[0], step=1, value=int(vol_shape[0] / 2)),
 
     )
+
+
+def show_3volume(vol, z, fig_size=(21, 7)):
+    #print(vol[0].shape)
+    #print(vol[1].shape)
+
+    fig, axarr = plt.subplots(nrows=1, ncols=3, figsize=fig_size)
+    v_z, v_y, v_x, n = vol[0].shape
+
+    axarr[0].imshow(vol[0][z, :, :, 0], cmap="gray")
+    axarr[0].set_title('Input (Low Resolution)', fontweight='bold', fontsize=18)
+    axarr[0].axis('off')
+
+    axarr[1].imshow(vol[1][z, :, :, 0], cmap="gray")
+    axarr[1].set_title('Target (High Resolution)', fontweight='bold', fontsize=20)
+    axarr[1].axis('off')
+
+    axarr[2].imshow(vol[2][z, :, :, 0], cmap="gray")
+    axarr[2].set_title('Generated Image', fontweight='bold', fontsize=20)
+    axarr[2].axis('off')
+
+    fig.tight_layout()
+
+
+def interactive_inference(vol):
+    volume= vol[0]
+    vol_shape = volume.shape
+    interact(
+        lambda z: plt.show(show_3volume(vol, z)),
+        z=IntSlider(min=0, max=vol_shape[0], step=1, value=int(vol_shape[0] / 2)),
+
+    )
+
+def mae_calc(y_true, predictions):
+    y_true, predictions = np.array(y_true), np.array(predictions)
+    return np.mean(np.abs(y_true - predictions))
